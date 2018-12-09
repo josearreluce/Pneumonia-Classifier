a4fa13435a2a05941a11e8651c79b0a0c5b9cc04
Jose Arreluce <arreluce.jose@gmail.com>
2018-12-09 16:01:53 -0600

    Updated data gathering files and gitignore.

--- gather_test.py
+++ gather_test.py
@@ -1,7 +1,9 @@
 from os import listdir
 from scipy.misc import imread
+from skimage.transform import resize
 import numpy as np
-import pickle
+
+import json
 
 test_images = []
 test_labels = []
@@ -12,14 +14,24 @@ pneumonia_test_directory = './data/test/PNEUMONIA/'
 print("Gathering Test Images")
 for filename in listdir(normal_test_directory):
     image = imread(normal_test_directory + filename)
-    test_images.append(np.array(image))
+    image = resize(image, (224, 224))
+    image = image.tolist()
+
+    test_images.append(image)
     test_labels.append(0)
 
 for filename in listdir(pneumonia_test_directory):
     image = imread(pneumonia_test_directory + filename)
-    test_images.append(np.array(image))
+    image = resize(image, (224, 224))
+    image = image.tolist()
+
+    test_images.append(image)
     test_labels.append(1)
 
 print("Finished Gathering Test Images")
 
-pickle.dump((test_images, test_labels), open('test_data', 'wb'))
+#pickle.dump((test_images, test_labels), open('test_data', 'wb'))
+data = {'images': test_images, 'labels': test_labels}
+print(len(test_images))
+with open('test_data.txt', 'w') as outfile:
+    json.dump(data, outfile)
--- gather_train.py
+++ gather_train.py
@@ -1,5 +1,6 @@
 from os import listdir
 from scipy.misc import imread
+from skimage.transform import resize
 import json
 
 pneumonia_training_directory = './data/train/PNEUMONIA/'
@@ -11,24 +12,24 @@ print("Gathering Training Images")
 pcount = 0
 for filename in listdir(pneumonia_training_directory):
     image = imread(pneumonia_training_directory + filename)
-    #pneumonia_training_images.append((image, 1))
+    image = resize(image, (224, 224))
+    image = image.tolist()
+
     training_images.append(image)
     training_labels.append(1)
-    print(pcount)
-    pcount += 1
+
 print("FINISHED GATHERING PNEUMONIA")
 ncount = 0
 for filename in listdir(normal_training_directory):
     image = imread(normal_training_directory + filename)
-    #normal_training_images.append((image, 0))
-    training_images.append(image.tolist())
+    image = resize(image, (224, 224))
+    image = image.tolist()
+
+    training_images.append(image)
     training_labels.append(0)
-    print(ncount)
-    ncount += 1
 print("FINISHED GATHERING NORMAL")
+
 data = {'images': training_images, 'labels': training_labels}
-#pickle.dumps((training_images, training_labels), open('train_data', "wb"), protocol=4)
+print(len(training_images))
 with open('train_data.txt', 'w') as outfile:
     json.dump(data, outfile)
-
-print("Finished Gathering Training Images")
