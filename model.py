60db1cc1c2df734afb482e6070653316471e5a94
josearreluce <arreluce.jose@gmail.com>
2018-12-16 00:14:29 +0000

    Use 2 conv2d layers.

--- model.py
+++ model.py
@@ -42,16 +42,17 @@ class model():
     def create_model(self):
         model = Sequential()
         model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(244, 244, 1)))
-#        model.add(Conv2D(32, (3, 3), activation='relu'))
+        model.add(Conv2D(32, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-#        model.add(Dropout(0.25))
+        model.add(Dropout(0.25))
 
         model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-#        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(128, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
